929ec97628691c82939ccb68cd477192a5f82aaf
mybabygrand <amuro_ray@kakao.com>
2022-06-21 22:17:09 +0900

    Section6. 데이터 접근 기술 - 스프링 데이터 JPA  - 스프링 데이터 JPA 시작

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -64,6 +65,7 @@ class ItemRepositoryTest {
     }
 
     @Test
+//    @Commit
     void updateItem() {
         //given
         Item item = new Item("item1", 10000, 10);
