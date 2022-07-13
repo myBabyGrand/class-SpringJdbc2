626d6b96b6f2ff539126d9bf5c313e6809cb1f9c
mybabygrand <amuro_ray@kakao.com>
2022-07-13 22:17:33 +0900

    Section11. 스프링 트랜잭션 전파2 - 활용  - 커밋, 롤백

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -35,10 +36,28 @@ class MemberServiceTest {
         memberService.joinV1(username);
 
         //then : 모든 데이터가 정상 저장
-        Assertions.assertTrue(memberRepository.find(username).isPresent());
-        Assertions.assertTrue(logRepository.find(username).isPresent());
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
 
     }
 
+    /**
+     * memberService : @Transactional - Off
+     * memberRepository : @Transactional - On
+     * logRepository : @Transactional - On Exception
+     * */
+    @Test
+    void outerTxOff_fail(){
+        //given
+        String username = "로그예외_outerTxOff_success";
+
+        //when
+        assertThatThrownBy(()-> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        //then : 모든 데이터가 정상 저장
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+
+    }
 
 }
